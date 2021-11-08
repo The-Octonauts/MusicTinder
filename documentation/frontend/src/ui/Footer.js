c0f327e4e50a3e840df8b7b43eddd81d4d5b1b21
aaron torres <atorres205@cnm.edu>
2021-11-08 16:44:22 -0700

    added footer and navbar

---
+++ documentation/frontend/src/ui/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Container} from "react-bootstrap";
+
+export function Footer () {
+    return(
+        <footer>
+            <Container fluid={'true'} className={'text-white bg-dark p-5'}>
+                <p className={'text-center'}> 2021 copyright</p>
+            </Container>
+        </footer>
+
+    )
+
+}
---
+++ documentation/frontend/src/ui/Navigation.js
@@ -0,0 +1,28 @@
+import  React from "react"
+import {Container, Nav, Navbar} from "react-bootstrap";
+import podpal from "../assets/Podpal_navbar.png"
+
+
+export function Navigation (){
+    return(
+        <Navbar collapseOnSelect expand="lg" bg="primary" variant="light">
+            <Container fluid>
+                <Navbar.Brand href="/home">   <img
+                    src={podpal}
+                    width="120"
+                    height="60"
+                    alt="Pod Pal logo"
+                /></Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse>
+                    <Nav className="me-auto "> </Nav>
+
+                    <Nav>
+                        <Nav.Link href="/about" >Inbox</Nav.Link>
+                        <Nav.Link href="/about" >Home</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
