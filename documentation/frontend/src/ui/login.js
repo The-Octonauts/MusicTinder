e717bc02e2dbf49f809ac399529762d2c2e87500
CLOVETHEDESTROYER <90019717+CLOVETHEDESTROYER@users.noreply.github.com>
2021-11-08 20:43:17 -0700

    added new logo and adjusted sizing

--- documentation/frontend/src/ui/login.js
+++ documentation/frontend/src/ui/login.js
@@ -2,7 +2,6 @@ import React from "react"
 import {Button, Col, Container, Image, Row} from "react-bootstrap";
 import podpal from "../assets/PodPal (1).png"
 import "./login.css"
-import background from "../assets/Podcast-audience.png";
 
 export const login = () => {
     return (
@@ -13,7 +12,7 @@ export const login = () => {
                     <Row>
                         <Col className="d-flex flex-column align-items-center">
                             <h1>Just Swipe It</h1>
-                            <Image src={podpal} alt="PodPals"/>
+                            <Image src={podpal} alt="PodPals" width="400px" height="400px"/>
                             <Button variant={"danger"} className={"rounded centered"}>Create Account</Button>
                             <h4>Meet friends who like the same Podcasts as you!</h4>
                         </Col>
