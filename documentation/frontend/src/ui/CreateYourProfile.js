8e198be4c05d5ab4e840b2a2a8653bf6540aa394
Alan Ramirez <aramireznicola@cnm.edu>
2021-11-08 22:00:30 -0700

    added form group

--- documentation/frontend/src/ui/CreateYourProfile.js
+++ documentation/frontend/src/ui/CreateYourProfile.js
@@ -2,6 +2,65 @@ import React from "react";
 import {Col, Form, Image, Row} from "react-bootstrap";
 
 export const CreateYourProfile = () => {
+    const [selected, setSelected] = React.useState("");
+    const changeSelectOptionHandler = (event) => {
+        setSelected(event.target.value);
+    };
+    const comedy = [
+        "The Joe Rogan Experience",
+        "Banter with Sapnap and Karl Jacobs",
+        "Morbid: A True Crime Podcast",
+        "Call Her Daddy",
+        "Armchair Expert Umbrella with Dax Shepard"
+    ];
+    const news = [
+        "The Ben Shapiro Show",
+        "The Daily",
+        "NPR Up First",
+        "Breaking Points with Krystal and Saagar",
+        "Morning Wire",
+    ];
+    const trueCrime = [
+        "Bruh Issa Murder",
+        "Wine & Crime",
+        "Bear Brook",
+        "Morbid: A True Crime Podcast",
+        "A Murder in My Family",
+    ];
+    const sport = [
+        "Pardon My Take",
+        "The Bill Simmons Podcast",
+        "The Rumor",
+        "The Pat McAfee Show 2.0",
+        "The Dan le Batard Show with Stugotz",
+    ];
+    const health = [
+        "Mind Pump",
+        "Sika Strength",
+        "Ben Greenfield Fitness",
+        "The Drive with Peter Attia",
+        "The Doctors Farmacy",
+    ];
+
+    let type = null;
+    let options = null;
+
+    if (selected === "Comedy") {
+        type = comedy;
+    } else if (selected === "News"){
+        type = news;
+    } else if (selected === "True Crime") {
+        type = trueCrime;
+    }else if (selected === "Sports") {
+        type = sport;
+    } else if (selected === "Health/Fitness") {
+        type = health
+    }
+
+    if (type) {
+        options = type.map((el) => <option key={el}>{el}</option>);
+    }
+
     return(
         <>
             <h1>Create Your Profile</h1>
@@ -26,79 +85,26 @@ export const CreateYourProfile = () => {
                 <h2>Please add your favorite podcasts</h2>
                 <div>
                     <Form>
-                        <Row>
+                        <div>
                             <Form.Group>
                                 <Form.Label>Genre</Form.Label>
-                                <Form.Select defaultValue="Choose">
-                                    <option>Choose*</option>
+                                <Form.Select onChange={changeSelectOptionHandler}>
+                                    <option>Choose</option>
                                     <option>Comedy</option>
                                     <option>News</option>
                                     <option>True Crime</option>
-                                    <option>Sport</option>
+                                    <option>Sports</option>
                                     <option>Health/Fitness</option>
                                 </Form.Select>
                             </Form.Group>
-                            {/*Comedy Podcasts*/}
-                            <Form.Group>
-                                <Form.Label>Podcasts</Form.Label>
-                                <Form.Select defaultValue="Choose">
-                                    <option>Choose*</option>
-                                    <option>The Joe Rogan Experience</option>
-                                    <option>Banter with Sapnap and Karl Jacobs</option>
-                                    <option>Morbid: A True Crime Podcast</option>
-                                    <option>Call Her Daddy</option>
-                                    <option>Armchair Expert Umbrella with Dax Shepard</option>
-                                </Form.Select>
-                            </Form.Group>
-                            {/*News*/}
-                            <Form.Group>
-                                <Form.Label>Podcasts</Form.Label>
-                                <Form.Select defaultValue="Choose">
-                                    <option>Choose*</option>
-                                    <option>The Ben Shapiro Show</option>
-                                    <option>The Daily</option>
-                                    <option>NPR Up First</option>
-                                    <option>Breaking Points with Krystal and Saagar</option>
-                                    <option>Morning Wire</option>
-                                </Form.Select>
-                            </Form.Group>
-                            {/*True Crime*/}
-                            <Form.Group>
-                                <Form.Label>Podcasts</Form.Label>
-                                <Form.Select defaultValue="Choose">
-                                    <option>Choose*</option>
-                                    <option>Bruh Issa Murder</option>
-                                    <option>Wine & Crime</option>
-                                    <option>Bear Brook</option>
-                                    <option>Morbid: A True Crime Podcast</option>
-                                    <option>The Murder in My Family</option>
-                                </Form.Select>
-                            </Form.Group>
-                            {/*Sport*/}
-                            <Form.Group>
-                                <Form.Label>Podcasts</Form.Label>
-                                <Form.Select defaultValue="Choose">
-                                    <option>Choose*</option>
-                                    <option>Pardon My Take</option>
-                                    <option>The Bill Simmons Podcast</option>
-                                    <option>The Rumor</option>
-                                    <option>The Pat McAfee Show 2.0</option>
-                                    <option>The Dan Le Batard Show with Stugotz</option>
-                                </Form.Select>
-                            </Form.Group>
-                            {/*Health/Fitness*/}
-                            <Form.Group>
-                                <Form.Label>Podcasts</Form.Label>
-                                <Form.Select defaultValue="Choose">
-                                    <option>Choose*</option>
-                                    <option>Mind Pump</option>
-                                    <option>Sika Strength </option>
-                                    <option>Ben Greenfield Fitness</option>
-                                    <option>The Drive with Peter Attia</option>
-                                    <option>The Doctor’s Farmacy</option>
-                                </Form.Select>
-                            </Form.Group>
-                        </Row>
+                        </div>
+                        <div>
+                            <Form.Select>
+                                {
+                                    options
+                                }
+                            </Form.Select>
+                        </div>
                     </Form>
                 </div>
             </section>
