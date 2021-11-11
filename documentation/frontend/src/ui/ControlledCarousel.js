dfd3342b1417a4e2cf189fa4c78e0c6c9203c02c
CLOVETHEDESTROYER <90019717+CLOVETHEDESTROYER@users.noreply.github.com>
2021-11-10 21:10:37 -0700

    gradient to carousel pic

--- documentation/frontend/src/ui/ControlledCarousel.js
+++ documentation/frontend/src/ui/ControlledCarousel.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import Carousel from "react-bootstrap/Carousel";
-import {Card, Image, ListGroup, ListGroupItem} from "react-bootstrap";
+import {Card, Image, ListGroup, ListGroupItem, Row} from "react-bootstrap";
 import man from "../assets/stylish-man-2021-09-24-03-15-41-utc.jpg";
 import girl1 from "../assets/woman-2021-09-01-13-01-13-utc.JPG";
 import girl2 from "../assets/woman-2021-09-01-13-02-54-utc.JPG";
@@ -16,25 +16,34 @@ export const ControlledCarousel = () => {
     return (
         <Carousel activeIndex={index} onSelect={handleSelect}>
             <Carousel.Item>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src={girl1}/>
+
+                <Card style={{ width: '20rem'}} className="text-white bg-dark">
+                    <Card.Img variant="top" src={girl1} className="bg-image"
+                         style={{
+                         }}/>
+                   {/* <Card.Img variant="top" src={girl1} alt="recommendation image" className="img-relative"/>*/}
                     <Card.Body>
-                        <Card.Title>Kyliee, 21</Card.Title>
-                        <Card.Text>
+                    <Card.ImgOverlay>
+                        <Row>
+                        <Card.Title className="text-absolute">Kyliee, 21</Card.Title>
+                        </Row>
+                        <Row>
+                        <Card.Text className="text-absolute">
                             Her favorite podcasts are Joe Rogan Experience and Consider This.
                         </Card.Text>
-                    </Card.Body>
-                    <ListGroup className="list-group-horizontal">
-                        <ListGroupItem>❌</ListGroupItem>
-                        <ListGroupItem>💖</ListGroupItem>
-                        <ListGroupItem>✅</ListGroupItem>
-                    </ListGroup>
+                        </Row>
+                    </Card.ImgOverlay>
+            </Card.Body>
                     <Card.Body>
+                        <ListGroup className="list-group-horizontal">
+                            <ListGroupItem>❌</ListGroupItem>
+                            <ListGroupItem>💖</ListGroupItem>
+                            <ListGroupItem>✅</ListGroupItem>
+                        </ListGroup>
                         <Card.Link href="#">Card Link</Card.Link>
                         <Card.Link href="#">Another Link</Card.Link>
                     </Card.Body>
                 </Card>
-
             </Carousel.Item>
 
         </Carousel>
