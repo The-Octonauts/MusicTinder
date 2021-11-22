a32acea62890a1aa0a717fd74cfaa14b5d84d47e
aaron torres <atorres205@cnm.edu>
2021-11-22 11:20:54 -0700

    revised sign-in

---
+++ backend/src/apis/sign-in/sign-in.validator.ts
@@ -0,0 +1,20 @@
+import {Schema} from 'express-validator';
+
+export const signInValidator : Schema = {
+    profilePassword: {
+        isLength: {
+            errorMessage: 'Password must be at least eight characters',
+            options: { min: 8 }
+        },
+        trim: true,
+        escape: true
+    },
+    profileEmail: {
+        isEmail: {
+            errorMessage: 'Please provide a valid email'
+        },
+        // Uncomment the next line to sanitize email, but it removes +1 from testing email addresses.
+        // normalizeEmail: true,
+        trim: true
+    }
+};
