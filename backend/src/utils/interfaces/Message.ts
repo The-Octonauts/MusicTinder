bc650d20f03eb8768cc407cde5a0e1c8e5feb7a1
CLOVETHEDESTROYER <90019717+CLOVETHEDESTROYER@users.noreply.github.com>
2021-11-29 15:31:11 -0700

    added all directories for sql tables

---
+++ backend/src/utils/interfaces/Message.ts
@@ -0,0 +1,6 @@
+export interface Message {
+    messageId: string;
+    messageeProfileId: string;
+    messageContent: string;
+    messageProfileId: string;
+}
--- backend/src/utils/saved/selectAllPodcastsBySavedProfileId.ts
+++ backend/src/utils/saved/selectAllPodcastsBySavedProfileId.ts
@@ -3,10 +3,10 @@ import {RowDataPacket} from 'mysql2';
 import {Show} from "../interfaces/Podcast";
 
 
-export async function selectAllPodcast(): Promise<Array<Show>> {
+export async function selectAllPodcastsBySavedProfileId(): Promise<Array<Show>> {
     try {
         const mysqlConnection = await connect();
-        const mysqlQuery: string = "SELECT BIN_TO_UUID(podcastId) as podcastId, podcastName, podcastGenre, podcastDescription, podcastImage  FROM podcast"
+        const mysqlQuery: string = "SELECT BIN_TO_UUID(savedPodcastId) as savedPodcastId, BIN_TO_UUID(savedProfileId) as savedProfileId  FROM saved"
         const result = await mysqlConnection.execute(mysqlQuery) as RowDataPacket[]
 
 
