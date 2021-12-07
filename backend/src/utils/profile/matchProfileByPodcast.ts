70f39f3b921c248199909b7f56e14a14d5c971f0
CLOVETHEDESTROYER <90019717+CLOVETHEDESTROYER@users.noreply.github.com>
2021-12-07 09:45:26 -0700

    updated match profile by podcast sql statement

--- backend/src/utils/profile/matchProfileByPodcast.ts
+++ backend/src/utils/profile/matchProfileByPodcast.ts
@@ -1,13 +1,27 @@
-/*import {connect} from "../database.utils";
+import {connect} from "../database.utils";
 import {Profile} from "../interfaces/Profile";
 import {RowDataPacket} from "mysql2";
 
-export async function matchProfileByPodcast(profileEmail: string): Promise<Profile|null> {
+export async function selectMatchesByProfileId(profile: Profile) : Promise<string>{
     try {
-        const profilePodcast = [pod]
+        const mysqlConnection = await connect();
+        const query = string = 'SELECT DISTINCT profileId FROM saved WHERE podcastId IN (SELECT podcastId FROM saved WHERE profileId= 'db605a6352f911ec83f90242ac180002') AND profileId != 'db605a6352f911ec83f90242ac180002'
 
         }
     } catch (error) {
         throw error
     }
-}*/
+}
+
+
+export async function selectMatchesByProfileId(profile: Profile) : Promise<string>{
+    try {
+        console.log("kghghjgkj",profile)
+        const mysqlConnection = await connect();
+        const query : string = 'INSERT INTO profile(profileId, profileActivationToken, profilePhotoUrl, profileEmail, profileHash, profilePhotoId) VALUES (UUID_TO_BIN(UUID()), :profileActivationToken, :profilePhotoUrl, :profileEmail, :profileHash, :profilePhotoId)';
+        await mysqlConnection.execute(query, profile);
+        return 'Profile Successfully Created'
+    } catch (error) {
+        throw error
+    }
+}
