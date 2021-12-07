8866349ce37248e896009944c6b6d5f12214af26
Alan Ramirez <aramireznicola@cnm.edu>
2021-12-07 08:48:16 -0700

    added matching to test

--- backend/src/utils/match/selectSavedPodcastIdToProfileId.ts
+++ backend/src/utils/match/selectSavedPodcastIdToProfileId.ts
@@ -1,12 +1,14 @@
 import {Profile} from "../interfaces/Profile";
 import {connect} from "../database.utils";
 import {RowDataPacket} from "mysql2";
+import {Saved} from "../interfaces/Saved";
+
 
 
 export async function selectSavedPodcastIdToProfileId (saved: Profile): Promise<Profile|null> {
     try {
         const mysqlConnection = await connect();
-        const mysqlQuery: string = "SELECT saved.savedProfileId, saved.savedPodcastId FROM saved WHERE saved.savedProfileId = savedProfileId = saved.savedProfileId order by profile.profileId)"
+        const mysqlQuery: string = "SELECT DISTINCT profileId FROM saved WHERE podcastId In (SELECT podcastID FROM saved Where profileId=25)AND profileId != 25"
         const result = await mysqlConnection.execute(mysqlQuery ,) as RowDataPacket[]
     }  catch (error) {
         throw error
